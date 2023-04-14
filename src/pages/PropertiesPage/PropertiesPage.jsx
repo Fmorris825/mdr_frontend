bc41ec68097f69229e096437ba9fa378a8389f49
Fred Morris <FredrickCPMorris@hotmail.com>
2023-04-14 11:56:38 -0500

    changed request to http request.

--- src/pages/PropertiesPage/PropertiesPage.jsx
+++ src/pages/PropertiesPage/PropertiesPage.jsx
@@ -21,7 +21,7 @@ const PropertiesPage = () => {
   async function getProperties() {
     try {
       const response = await axios.get(
-        `https://www.morrisdeltarealty.com/api/properties/`
+        `http://mdrbackendcapstone-env.eba-tgn5xhjt.us-east-1.elasticbeanstalk.com/api/properties/`
       );
       setProperties(response.data);
     } catch (error) {
